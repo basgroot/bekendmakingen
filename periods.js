017eae74a3ce610817a20936f95ca0375744ba13
bas <bas.groot@basement.nl>
2024-01-01 10:45:14 +0100

    Add December 2023

--- periods.js
+++ periods.js
@@ -12,6 +12,9 @@ const periods = [
         "key": "all",
         "val": "Alle recente publicaties"
     }, {
+        "key": "2023-12",
+        "val": "December 2023"
+    }, {
         "key": "2023-11",
         "val": "November 2023"
     }, {
